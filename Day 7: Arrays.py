6d24c8f709b4c113f2f3ecbfe6b0241e3034af9e
VeronicaGupta <39666569+VeronicaGupta@users.noreply.github.com>
2019-01-24 00:23:26 +0530

    Task
    Given an array, A, of N integers, print A's elements in reverse order as a single line of space-separated numbers.

---
+++ Day 7: Arrays.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+    x=arr[n::-1]
+    print(x)
+    print(type(split(x))
